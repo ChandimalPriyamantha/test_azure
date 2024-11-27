a76e259d95a6389715227c1aee87bb43e2f8e2b2
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2024-11-28 03:39:46 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,35 @@
+def Articles():
+    articles = [
+
+
+        {
+
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        },
+
+
+
+        {
+
+            'id': 2,
+            'title':'Article Tow',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        }, 
+
+        {
+
+            'id': 3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        }     
+    ]
+
+    return articles
